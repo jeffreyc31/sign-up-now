bf9bd67d4382951bc86e4201d125122234b7956a
jeffreyc31 <jeffcgit31@gmail.com>
2022-04-28 13:59:50 -0700

    add function to store user password in the confirm password field

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 let pass = document.getElementById('pass-word');
-let confirm = document.getElementById('confirm-pass');
+let confirmed = document.getElementById('confirm-pass');
 let first = '';
 let second = '';
 let deleted = '';
+let confirmOne = '';
+let confirmSecond = '';
+let confirmDel = '';
 
 
-pass.addEventListener('keypress', updatePass);
+pass.addEventListener('keydown', updatePass);
+confirmed.addEventListener('keydown', updatePass);
 
 function updatePass(e) {
     if (e.keyCode == 8 && first != '') {
         deleted = first.slice(0, first.length - 1);
         first = deleted;
-        console.log(first);
     } else if (e.keyCode == 8 && first == '') {
         return;
     } else {
         first = first + (String.fromCharCode(e.keyCode));
         console.log(first);
     }
+}
+
+function updateConfirm(e) {
+    if (e.keyCode == 8 && confirmOne != '') {
+        confirmDel = confirmOne.slice(0, confirmOne.length - 1);
+        confirmOne = confirmDel;
+    } else if (e.keyCode == 8 && confirmOne == '') {
+        return;
+    } else {
+        confirmOne = confirmOne + (String.fromCharCode(e.keyCode));
+    }
+
 }
